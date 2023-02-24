31eb9c6a02f311e975111345037a31f7d76aa601
Vaibhav Gupta <guptavaibhav432@gmail.com>
2023-02-24 23:00:06 +0530

    MCQS done Agagin

--- src/assets/Constants.js
+++ src/assets/Constants.js
@@ -1,6 +1,25 @@
 export const darkTheme = {
-    bgPrimary: "#222629",
-    bgSecondary: "#6B5DD3",
-    bgBox: "#222629",
-    borderPrimary: "#BDA9A9",
+	bgPrimary: "#222629",
+	bgSecondary: "#6B5DD3",
+	bgBox: "#222629",
+	borderPrimary: "#BDA9A9",
 };
+
+export const topics = [
+	{ title: "All Questions" },
+	{ title: "Arrays" },
+	{ title: "Strings" },
+	{ title: "Linked List" },
+	{ title: "Stacks" },
+	{ title: "Queues" },
+	{ title: "Trees" },
+	{ title: "Graphs" },
+	{ title: "Sorting" },
+	{ title: "Searching" },
+	{ title: "Dynamic Programming" },
+	{ title: "Greedy" },
+	{ title: "Backtracking" },
+	{ title: "Bit Manipulation" },
+	{ title: "Math" },
+	{ title: "Miscellaneous" },
+];
